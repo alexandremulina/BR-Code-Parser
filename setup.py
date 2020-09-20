d3675a243c787e0bf3fc24b53427e6954b0ecfee
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2020-09-20 17:17:46 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name='brqr',
+      version='1.0',
+      description='Extração de dados BR CODE',
+      url='https://github.com/alexandremulina/brqr',
+      author='Hudson Brendon',
+      author_email='alexandremulina@hotmail.com',
+      license='MIT',
+      packages=['brqr'],
+      zip_safe=False)
