afd95633cec371fd75dcf2e97234e57be5fcbfa5
alexandremulina <49921028+alexandremulina@users.noreply.github.com>
2020-09-20 22:11:43 -0300

    Add files via upload

---
+++ brcode/__init__.py
@@ -0,0 +1,13 @@
+from .brcode import *
+
+
+__short_description__ = (
+    "Extração valores BRCODE - "
+    
+)
+__license__ = "MIT"
+__author__ = "Alexandre Mulina"
+__author_email__ = "alexandremulina@hotmail.com"
+__maintainer__ = "Alexandre Mulina"
+__maintainer_email__ = "alexandremulina@hotmail.com"
+__github_username__ = "alexandremulina"
